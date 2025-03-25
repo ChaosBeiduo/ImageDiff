431056e0de474843a33a83bb548602cf7ebd50bd
Chako <chaosbeiduo@outlook.com>
2025-03-25 12:42:16 +0800

    refactor: remove vue project to folder 'frontend'

---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>ImageDiff</h1>
+    <p>This is an screenshots comparison website</p>
+  </header>
+</template>
+<script setup lang="ts">
+
+</script>
+
+<style>
+header{
+  text-align: center;
+  background: white;
+}
+</style>
